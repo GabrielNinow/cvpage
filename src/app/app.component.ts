c47091aa706ba24e475c986ef57679f5425ea3f0
gabriel ninow <gabrielninow@gmail.com>
2024-01-28 20:07:55 -0300

    fixed: index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,4 @@ import { RouterOutlet } from '@angular/router';
   imports: [CommonModule, RouterOutlet],
   template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'cvpage';
-}
+export class AppComponent { }
