6645d467dfcc1c3223342a5819b1641e2d4fc125
gabriel ninow <gabrielninow@gmail.com>
2024-01-30 15:04:39 -0300

    fixed some typos

--- src/app/modules/cv/components/knowledge/knowledge.component.ts
+++ src/app/modules/cv/components/knowledge/knowledge.component.ts
@@ -12,23 +12,38 @@ export class KnowledgeComponent {
   public arrayKnowledge = signal<IKnowledge[]>([
     {
       src: 'assets/icons/knowledge/html5.svg',
-      alt: 'knowledge html5 icon'
+      alt: 'knowledge html5 icon',
+      title: 'HTML5',
     },
     {
       src: 'assets/icons/knowledge/css3.svg',
-      alt: 'knowledge css3 icon'
+      alt: 'knowledge css3 icon',
+      title: 'CSS3',
     },
     {
       src: 'assets/icons/knowledge/javascript.svg',
-      alt: 'knowledge javascript icon'
+      alt: 'knowledge javascript icon',
+      title: 'Javascript',
     },
     {
       src: 'assets/icons/knowledge/angular.svg',
-      alt: 'knowledge angular icon'
+      alt: 'knowledge angular icon',
+      title: 'Angular',
     },
     {
       src: 'assets/icons/knowledge/nodejs.svg',
-      alt: 'knowledge nodejs icon'
+      alt: 'knowledge nodejs icon',
+      title: 'Node',
+    },
+    {
+      src: 'assets/icons/knowledge/csharp.svg',
+      alt: 'knowledge c# icon',
+      title: 'C#',
+    },
+    {
+      src: 'assets/icons/knowledge/mysql.svg',
+      alt: 'knowledge mysql icon',
+      title: 'MySQL',
     },
   ]);
 }
--- src/app/modules/cv/components/projects/projects.component.ts
+++ src/app/modules/cv/components/projects/projects.component.ts
@@ -15,16 +15,44 @@ export class ProjectsComponent {
   #dialog = inject(MatDialog);
   public arrayProjects = signal<IProjects[]>([
     {
-      src: 'assets/img/github.svg',
+      src: 'assets/img/projects/github.svg',
       alt: 'Github Repository',
       title: 'Github Repository',
       width: '100',
-      height: '51',
-      description: 'Github repository where you can find my portifolio projects',
+      height: '80',
+      description: 'Github repository where you can find my portifolio projects and source code of the another projects',
       links: [
         {
           name: 'Github',
           href: 'https://github.com/GabrielNinow',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/todo-list.png',
+      alt: 'To do list portifolio',
+      title: 'To do list portifolio',
+      width: '100',
+      height: '80',
+      description: 'one of my most recently projects, i made when i was learning angular. Further i will create a back-end using asp.net to save task in a database',
+      links: [
+        {
+          name: 'To-do list',
+          href: 'https://gabrielninow.github.io/Todo-List/browser/',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/acme-logo.png',
+      alt: 'Acme website portifolio',
+      title: 'Acme website',
+      width: '100',
+      height: '51',
+      description: 'one of my older projects, i did when i was in vocational school, and remade recently, with my actual skills using only HTML. CSS, Javascript and Bootstrap',
+      links: [
+        {
+          name: 'Acme Website',
+          href: 'https://gabrielninow.github.io/ACME-Website/',
         }
       ]
     }
